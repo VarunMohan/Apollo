9bd59112dfe754df19eb5d41178e6076e1490ada
Rahul Sridhar <rsridhar@mit.edu>
2016-04-16 16:14:04 -0400

    setup

---
+++ evoting/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name             = 'evoting',
+  version          = '0.1.0',
+  description      = '',
+  author           = '',
+  author_email     = '',
+  packages         = [
+  ],
+  install_requires = [
+    'phe == 1.2.2'
+  ]
+)
