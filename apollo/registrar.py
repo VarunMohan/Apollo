594639697e9a71df56cc3b56d0900ea3ba022aa4
vmohan <vmohan@mit.edu>
2016-04-24 18:56:00 -0400

    Removed Locking

--- apollo/registrar.py
+++ apollo/registrar.py
@@ -4,33 +4,24 @@ class Registrar:
     def __init__(self, election):
         self.election = election
         self.table = {}
-        self.lock = threading.Lock()
         self.done = False
 
     def add_voter(self, voter_id, vote):
-        self.lock.acquire()
         if voter_id in self.table or self.done:
-            self.lock.release()
             return False
         record = VoterRecord(vote, False)
         self.table[voter_id] = record
-        self.lock.release()
         return True
 
     def confirm_vote(self, voter_id, vote):
-        self.lock.acquire()
         record = self.table[voter_id]
         if record.has_voted or record.vote != vote:
-            self.lock.release()
             return False
         record.has_voted = True
-        self.lock.release()
         return True
 
     def voting_complete(self):
-        self.lock.acquire()
         self.done = True
-        self.lock.release()
 
 
 
