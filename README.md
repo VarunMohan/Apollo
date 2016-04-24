203d312bcc89bd117f92e033488615a18d541932
Varun Mohan <varunkmohan@gmail.com>
2016-04-24 15:00:13 -0400

    Name Change for Rahul

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# e-voting
+# Apollo 
 
 ```
 source env/bin/activate
--- apollo/setup.py
+++ apollo/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-  name             = 'e-voting',
+  name             = 'apollo',
   version          = '0.1.0',
   description      = '',
   author           = '',
