c0b83ee150fa097b19d8f1480984265eedc8a6c7
Varun Mohan <vmohan@mit.edu>
2016-04-24 15:15:14 -0400

    Added registar

--- apollo/registrar.py
+++ apollo/registrar.py
@@ -1,1 +1,22 @@
+class Registrar:
+    def __init__(self):
+        self.table = {}
 
+    def add_voter(self, voter_id, vote):
+        if voter_id in self.table:
+            return False
+        record = VoterRecord(vote, False)
+        self.table[voter_id] = record
+        return True
+
+    def confirm_vote(self, voter_id, vote):
+        if record.has_voted:
+            return False
+        record.has_voted = True
+        return True
+
+    
+class VoterRecord:
+    def __init__(self, vote, has_voted):
+        self.vote = vote
+        self.has_voted = has_voted
