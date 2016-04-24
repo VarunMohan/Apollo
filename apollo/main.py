9cc0a54faf4cbf6fb6497bc12ec600fd61dcf2e4
Rahul Sridhar <rsridhar@mit.edu>
2016-04-24 15:20:27 -0400

    main and voter logic done

--- apollo/main.py
+++ apollo/main.py
@@ -1,5 +1,21 @@
 from tallier import Tallier
+from registrar import Registrar
+from voter import Voter
+
+import random
 
 if __name__ == '__main__':
-    t = Tallier() 
-    print t.tally_votes()
+    t = Tallier()
+    r = Registrar()
+
+    voters = [Voter(i, r, t) for i in range(100)]
+    total = 0
+    for voter in voters:
+      vote = random.randint(0,10)
+      total += vote
+      voter.vote(vote)
+
+    result = t.tally_votes()
+    assert result == total
+
+    print("Everything is going swimmingly")
--- apollo/voter.py
+++ apollo/voter.py
@@ -1,1 +1,13 @@
+from registrar import Registrar
 
+class Voter:
+    def __init__(self, voter_id, registrar, tallier):
+        self.voter_id = voter_id
+        self.registrar = registrar
+        self.tallier = tallier
+
+    def vote(vote):
+        if self.registrar.add_voter(voter_id, vote):
+            if self.tallier.send_vote(voter_id, vote):
+                return True
+        return False
