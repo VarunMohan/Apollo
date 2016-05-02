b0185078216809d07e9527bf84260c6ab241d4ef
Rahul Sridhar <rsridhar@mit.edu>
2016-05-02 15:31:55 -0400

    Certificate integration works

--- apollo/demo.py
+++ apollo/demo.py
@@ -2,11 +2,13 @@ from client_tallier import ClientTallier
 from client_registrar import ClientRegistrar
 from client_authority import ClientAuthority
 import entity_locations
-import sys
 from voter import Voter
 
 import pickle
 import random
+import sys
+import hashlib
+from string import ascii_lowercase
 from flask import Flask, render_template, request, session, redirect
 from flaskext.xmlrpc import XMLRPCHandler, Fault
 
@@ -27,7 +29,7 @@ def submit_vote():
     message = 'Success!'
     try:
         t = ClientTallier(random.choice(tallier_endpoints))
-        voter = Voter('rsridhar', r, t, e)
+        voter = Voter(session['username'], r, t, e)
         success = voter.vote(request.form['candidate'])
         if not success:
             message = "Something went wrong, please try again"
@@ -51,15 +53,42 @@ def end_election():
 
     return message
 
-
-
-
 @app.route('/')
 def hello_world():
-    if r.is_election_running(eid):
-        return render_template('demo_interface.html')
+    if 'username' in session:
+        if not r.is_election_running(eid):
+            return render_template('demo_results.html', username = session['username'])
+        else:
+            return render_template('demo_interface.html', username = session['username'])
+    elif 'email' in request.args:
+        # coming back from auth.php (we think)
+        email = request.args['email']
+        token = request.args['token']
+        name = request.args['name']
+
+        key = session['key']
+        secret = 'password'
+        to_hash = (email + key + secret).encode('utf-8')
+        correct_token = hashlib.sha256(to_hash).hexdigest()
+
+        if token == correct_token:
+            message = 'Success!'
+            session['username'] = email[:-8]
+            print(session['username'])
+        else:
+            message = 'Authentication Failed'
+        print(message)
+
+        if not r.is_election_running(eid):
+            return render_template('demo_results.html', username = session['username'])
+        else:
+            return render_template('demo_interface.html', username = session['username'])
     else:
-        return render_template('demo_results.html')
+        authUrl = 'https://rsridhar.scripts.mit.edu:444/auth.php'
+        key = ''.join(random.choice(ascii_lowercase) for i in range(10))
+        linkUrl = authUrl + '?key=' + key
+        session['key'] = key
+        return redirect(linkUrl)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8192, debug=False)
